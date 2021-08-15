4393b1ca1f6b809cc36ec8389cb840e470a5c727
kibi11 <arpansinghranchi@gmail.com>
2021-08-15 15:01:46 +0530

    add the Info card & Bar of Progress npm
    added the styling to the INfo card section : however could be better in mobile view and added the npm/bar-of-progress functionality

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,26 +1,40 @@
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/solid";
+import { HeartIcon } from "@heroicons/react/outline";
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div className="bg-black">
-      <div className="flex relative p-5 h-100 mt-5 bg-pink-100 rounded-lg">
-        <div className="relative h-60 w-80 p-5">
-          <Image src={img} layout="fill" className="rounded-lg" />
+    <div className="flex px-2 py-7 border-b hover:opacity-80 hover:shadow-lg transition transform duration-100 ease-out cursor-pointer first:border-t">
+      <div className="relative h-30 w-40 md:h-52 md:w-80 flex-shrink-0">
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+        />
+      </div>
+
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <h3>{location}</h3>
+          <HeartIcon className="h-7 cursor-pointer" />
+        </div>
+        <h1 className="text-xl ">{title}</h1>
+        <div className="border-b w-10 pt-2" />
+
+        <p className="text-gray-400 pt-2 text-sm flex-grow">{description}</p>
+        <div className="flex justify-end text-xl font-semibold pb-2 lg:text-2xl">
+          <p>{price}</p>
         </div>
 
-        <div className="relative h-60 w-80 ">
-          <h3>{location}</h3>
-          <h1 className="font-semibold ">{title}</h1>
-          <p className="text-gray-400">{description}</p>
-          <p className="flex">
+        <div className="flex justify-between pt-2">
+          <p className="flex items-center">
             <p>
               <StarIcon className="h-5 text-[#FFA500]" />
             </p>
             <p>{star}</p>
           </p>
-          <h1>{price}</h1>
-          <h2>{total}</h2>
+          <p className="font-extralight">{total}</p>
         </div>
       </div>
     </div>
